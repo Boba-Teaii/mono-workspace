5b7b84ff40c1d703b772cfb337451e8d444a2e1d
Boba-Teaii <chaitanyakashyap69@gmail.com>
2026-04-01 22:17:37 +0530

    new file:   "\360\237\220\215python-projects/if-else.py"

---
+++ 🐍python-projects/if-else.py
@@ -0,0 +1,7 @@
+user_input = input("Enter a number: ")
+if user_input == "1":
+    print("You entered one.")
+elif user_input == "2":
+    print("You entered two.")
+else:
+    print("You entered something else.")
